96e2db57a075e28105edeb33259f49810f7a4148
Stefan Safranek <stefan@stefan>
2017-03-29 11:09:53 -0700

    css animation

--- L.AnimatedPath.js
+++ L.AnimatedPath.js
@@ -10,7 +10,6 @@ L.AnimatedPath = L.Polyline.extend({
 		opacity: 0.5,
 		smoothFactor: 1,
 		dashArray: "5, 7",
-		//animationSpeed: 10000,
 		maxPathLength: 300		// removes old latlng values
 	},
 	
@@ -18,15 +17,7 @@ L.AnimatedPath = L.Polyline.extend({
         L.Util.setOptions(this, {});
 		L.Polyline.prototype.initialize.call(this, latlngs, options);
 	},
-/*	
-	setAnimationSpeed: function(milliseconds) {
-		this.options.animationSpeed = milliseconds;
-	},
-	
-	getAnimationSpeed: function() {
-		return this.options.animationSpeed;
-	},
-*/		
+
 	getMaxPathLength: function() {
 		return this.options.maxPathLength;
 	},
@@ -100,43 +91,5 @@ L.AnimatedPath = L.Polyline.extend({
 	*/
 		this.options.smoothFactor = smoothFactor;
 	}
-	
-/*
-	animate: function() {
-		var self = this;
-		
-		this._path.classList.add("animated-path");
-
-	
-		// Get animation speed.
-		var ms = this.getAnimationSpeed();
-		
-		// Reset transition to animate in the correct direction
-		// of the LineString feature.
-		this._path.style.transition = "";
-		
-		// Get length of svg path.
-		var totalLength = this._path.getTotalLength();
-		//this._path.style.strokeDashoffset = totalLength;
-		this._path.style.strokeDashoffset = 0;
-		//this._path.style.strokeDasharray = totalLength/100;
-
-		// Set transition for animation.
-		this._path.style.transition = "stroke-dashoffset "+ms+"ms linear";
-		
-		// Offset the timeout here: setTimeout makes a function
-		// run after a certain number of milliseconds - in this
-		// case we want each flight path to be staggered a bit.
-		setTimeout((function(path) {
-			return function() {
-				// setting the strokeDashoffset to 0 triggers
-				// the animation.
-				//path.style.strokeDashoffset = 0;
-				path.style.strokeDashoffset = totalLength;
-				self.animate();
-			};
-		})(this._path), ms);	
-	}
-*/
 
 });
